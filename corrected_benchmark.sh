cca1116084e451b7c3b0fdb3cd9feef503c2a99e
Lukas Kröger <LukasPKroeger@web.de>
2024-03-04 15:57:25 +0100

    Fixed Linking; Implement time formatting

--- corrected_benchmark.sh
+++ corrected_benchmark.sh
@@ -8,10 +8,15 @@ for delay in $(LANG=en_US seq 0.1 0.1 0.5)
 do
     for p in $(LANG=en_US seq 0.1 0.1 1)
     do
-        for run in $(seq 1 5)
+        for run in $(seq 16 20)
         do
-            echo " ./bin/darp_cplex_6 no6 -p $p --node-delay $delay &> ${dir}/dl${delay}p${p}_run${run}.txt"
-            ./bin/darp_cplex_6 no6 -p $p --node-delay $delay &> ${dir}/${delay}min${p}pr_run${run}.txt &
+            output_file="${dir}/${delay}min${p}pr_run${run}.txt"
+            if [ ! -f "$output_file" ]; then
+                echo " ./bin/darp_cplex_6 no6 -p $p --node-delay $delay &> $output_file"
+                ./bin/darp_cplex_6 no6 -p $p --node-delay $delay &> $output_file &
+            else
+                echo "Output file $output_file already exists, skipping..."
+            fi
 
             # If the number of background processes has reached $max_processes, wait until one finishes
             while [ $(jobs -p | wc -l) -ge $max_processes ]; do
